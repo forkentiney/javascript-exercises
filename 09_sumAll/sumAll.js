d18666c3da546c40fe57fec681573c315e2ac6b5
Forkentiney <jseverud@proton.me>
2025-12-05 10:28:00 -0600

    Assignment completed successfully.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+  let first = (x < y) ? x:y;
+  let last = (x < y) ? y:x;
+  let total = 0
+  if ((x || y) < 0) {
+    return "ERROR";
+  } else if (isNaN(x) || isNaN(y)) {
+    return "ERROR";
+  } else {
+  for (let i = first; i <= last; i++) {
+    total += i;
+  }
+  return total;
+  }
 };
 
 // Do not edit below this line
